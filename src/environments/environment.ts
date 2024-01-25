ad36743f58a392aec9a4e14c20b1daf95dc7229b
Fabio Rojas <Fabio.Rojas@softipal.com>
2024-01-25 13:40:21 -0500

    creation of the environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    apiUrlBase : `https://recruiting-api.newshore.es/api/flights/2`,
+    apiUrlCurrency : `https://v6.exchangerate-api.com/v6/1d789407b20503169daea9f3/latest/USD`,
+    production: true
+};
