fc011b3f720833b3e24f843030077084d3d3af5c
Fabio Rojas <Fabio.Rojas@softipal.com>
2024-01-25 07:05:24 -0500

    component rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CarouselComponent } from './component/carousel/carousel.component';
-import { FormComponent } from './component/form/form.component';
+import { FormComponent } from './components/form/form.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CarouselComponent, FormComponent],
+  imports: [RouterOutlet, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
